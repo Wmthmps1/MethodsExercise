186928a026b1762f0a50d12430de3f82b32382ba
William Thompson <wmthmps1@gmail.com>
2023-12-08 10:48:09 -0600

    Initial Commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,29 @@
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Time for a fun story:");
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine;
+            Console.WriteLine("What would your ideal superhero suffix be? For example: the suffix for Spiderman would be man.");
+            var suffix = Console.ReadLine;
+            Console.WriteLine("Enter your favorite animal: ");
+            var favoriteAnimal = Console.ReadLine;
+            Console.WriteLine("Enter your least favorite animal: ");
+            var leastFavAnimal = Console.ReadLine;
+            Console.WriteLine("What superpower would you like to have? ");
+            var superpower = Console.ReadLine;
+            string superheroName = String.Join(" ", favoriteAnimal, suffix);
+            string villain = String.Join(" ", "Dr.", leastFavAnimal);
+
+
+            Console.WriteLine($"One day, {name} was out in the woods and was attacked by a radioactive {favoriteAnimal}." +
+                $"{name} passed out, but woke up the next day to discover they had superpowers. {name} was now {superheroName}!" +
+                $"{superheroName} was out on the town when there was an attack. They looked up only to see {villain}." +
+                $"{superheroName} has to stop {villain}! So, using the power of {superpower}, {superheroName} was able to save the day!");
         }
+
     }
 }
